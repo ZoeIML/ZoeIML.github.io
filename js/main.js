0518ccf3c10ed53b524d1cdc1a79b806dc74d3e0
Zoe Lisk <zoelisk@Zoes-macbook-2.local>
2018-03-12 12:43:55 +1300

    js and buttons added

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//Get Random Acts of Kindness Link: 
+document.getElementById('button1').addEventListener("click", goKindness);
+
+function goKindness() {
+    location.href="https://zoeiml.github.io/kindness-generator"
+}
+
+//Get Calculator Link:
+document.getElementById('button2').addEventListener("click", goCalc); 
+
+function goCalc (){
+    location.href="https://zoeiml.github.io/js-calculator/"
+}
+
+//Get Minesweeper Link 
+document.getElementById('button3').addEventListener("click", goMinesweeper);
+
+function goMinesweeper() {
+    location.href="https://zoeiml.github.io/minesweeper"
+}
+
+//Make buttons fade in and out with mouseover 
